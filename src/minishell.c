c10c67feeea150bc6cb39806b8bff96577fc8275
Kilian Tintim Goncalves <ktintim-@c2r5s3.42lausanne.ch>
2025-02-03 15:31:17 +0100

    test fini

--- src/minishell.c
+++ src/minishell.c
@@ -3,14 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: psoulie <psoulie@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ktintim- <ktintim-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 10:22:23 by ktintim-          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2025/02/03 15:24:02 by psoulie          ###   ########.fr       */
-=======
-/*   Updated: 2025/02/03 15:25:04 by ktintim-         ###   ########.fr       */
->>>>>>> kilian
+/*   Updated: 2025/02/03 15:31:07 by ktintim-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,15 +26,6 @@ int	main(int ac, char **av, char **env)
 		splited = ft_split(input, '|');
 		if (splited == NULL)
 			error("Error: split failed");
-<<<<<<< HEAD
-		coucou ;
-=======
-		error("Error: split failed");
-		bonjour
-		rl_insert_comment
-		callocv
-		va_arg
->>>>>>> kilian
 		usleep(50);
 	}
 }
