3f4bf2e65cf31c28d29053fc165c7e37e09f07c6
Kilian Tintim Goncalves <ktintim-@c2r5s1.42lausanne.ch>
2025-03-07 14:43:19 +0100

    expension 0 added

--- src/utils/check_input.c
+++ src/utils/check_input.c
@@ -6,7 +6,7 @@
 /*   By: ktintim- <ktintim-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/21 12:00:16 by ktintim-          #+#    #+#             */
-/*   Updated: 2025/03/07 11:50:37 by ktintim-         ###   ########.fr       */
+/*   Updated: 2025/03/07 14:32:38 by ktintim-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,28 +30,81 @@ static char	*ft_getenv(char *key, char **env)
 	return (NULL);
 }
 
-static char	*new_input(char *input, int i, int j, char **env)
+static char	*replace_mark(char *input, int i, int ms_status)
 {
 	char	*new_input;
 	char	*beg;
-	char	*new;
-	char	*name_env;
 	char	*value;
+	char	*suffix;
 
-	beg = ft_substr(input, 0, i);
-	name_env = ft_substr(input, i + 1, j - i - 1);
-	value = ft_getenv(name_env, env);
-	new = ft_strjoin(beg, value);
+	if (i != 0)
+		beg = ft_substr(input, 0, i);
+	else
+		beg = ft_strdup("");
+	if (!beg)
+		return (NULL);
+	value = ft_itoa(ms_status);
+	if (!value)
+		return (free(beg), NULL);
+	new_input = ft_strjoin(beg, value);
+	if (!new_input)
+		return (NULL);
+	if (input[i + 2])
+		suffix = ft_strdup(&input[i + 2]);
+	else
+		suffix = ft_strdup("");
+	if (!suffix)
+		return (free(beg), free(value), free(new_input), NULL);
+	input = ft_strjoin(new_input, suffix);
+	return (free(beg), free(value), free(new_input), free(suffix), input);
+}
+
+static char	*get_expanded_input(char *input, int i, int j, char *expanded_value)
+{
+	char	*new_input;
+	char	*beg;
+	char	*temp;
+
+	if ((i != 0))
+		beg = ft_substr(input, 0, i);
+	else
+		beg = ft_strdup("");
+	if (!beg)
+		return (NULL);
+	temp = ft_strjoin(beg, expanded_value);
 	free(beg);
-	free(name_env);
-	free(value);
-	new_input = ft_strjoin(new, &input[j]);
-	free(new);
+	if (!temp)
+		return (NULL);
+	if (input[j])
+		new_input = ft_strjoin(temp, &input[j]);
+	else
+		new_input = ft_strdup(temp);
+	free(temp);
 	free(input);
 	return (new_input);
 }
 
-char	*found_dollar(char *input, char **env)
+static char	*replace_variable(char *input, int i, int j, char **env)
+{
+	char	*name_env;
+	char	*expanded_value;
+	char	*new_input;
+
+	name_env = ft_substr(input, i + 1, j - i - 1);
+	if (!name_env)
+		return (NULL);
+	expanded_value = ft_getenv(name_env, env);
+	free(name_env);
+	if (!expanded_value)
+		expanded_value = ft_strdup("");
+	if (!expanded_value)
+		return (NULL);
+	new_input = get_expanded_input(input, i, j, expanded_value);
+	free(expanded_value);
+	return (new_input);
+}
+
+char	*found_dollar(char *input, char **env, int ms_status)
 {
 	int	i;
 	int	j;
@@ -62,14 +115,17 @@ char	*found_dollar(char *input, char **env)
 		if (input[i] == '$')
 		{
 			j = i;
-			while (input[j] && input[j] != ' ')
-				j++;
-			if (j - i > 1)
-			{
-				input = new_input(input, i, j, env);
-			}
+			if (input[j + 1] == '?')
+				input = replace_mark(input, i, ms_status);
 			else
-				input[i] = ' ';
+			{
+				while (input[j] && input[j] != ' ')
+					j++;
+				if (j - i > 1)
+					input = replace_variable(input, i, j, env);
+				else
+					input[i] = ' ';
+			}
 		}
 		i++;
 	}
